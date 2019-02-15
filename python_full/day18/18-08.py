d68b732947e4aaeaad2e098dcd67ab78b1c2cf0c
Li Peng <lpsddz@126.com>
2019-02-15 09:02:27 +0800

    莫烦python和机器学习记录

--- python_full/day18/18-08.py
+++ python_full/day18/18-08.py
@@ -1,15 +1,30 @@
 import configparser
 config = configparser.ConfigParser()
-config["DEFAULT"] = {"ServerAliveInterval":"45",
-                     "Compression":"yes",
-                     "CompressionLevel":"9"}
-config["bitbucket.org"] = {}
-config['bitbucket.org']['User'] = 'hg'
-config['topsecret.server.com'] = {}
-topsecreter = config['topsecret.server.com']
-topsecreter['Host Port'] = '50022'
-topsecreter['ForwardX11'] = 'no'
-config['DEFAULT']['ForwardX11'] = 'yes'
+# config["DEFAULT"] = {"ServerAliveInterval":"45",
+#                      "Compression":"yes",
+#                      "CompressionLevel":"9"}
+# config["bitbucket.org"] = {}
+# config['bitbucket.org']['User'] = 'hg'
+# config['topsecret.server.com'] = {}
+# topsecreter = config['topsecret.server.com']
+# topsecreter['Host Port'] = '50022'
+# topsecreter['ForwardX11'] = 'no'
+# config['DEFAULT']['ForwardX11'] = 'yes'
+#
+# with open("config.ini","w") as conf:
+#     config.write(conf)
 
-with open("config.ini","w") as conf:
-    config.write(conf)
+config.read("config.ini")
+# config.sections()
+# print(config["DEFAULT"]["serveraliveinterval"])
+# for key in config["DEFAULT"]:
+#     print(key,"\t",config["DEFAULT"][key])
+config.set("DEFAULT","compression",'no')
+config.write(open("config.ini","w"))
+
+# config.remove_section("topsecret.server.com")
+# config.write(open("config_remove.ini","w"))
+
+
+
+
