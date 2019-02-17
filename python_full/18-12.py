fe971e22564df468f8a4d7cd233d4e1b3b3a5338
Li Peng <lpsddz@126.com>
2019-02-17 13:13:32 +0800

    莫烦python和机器学习记录

---
+++ python_full/18-12.py
@@ -0,0 +1,25 @@
+import re
+
+# ret = re.split('s','asdfose')
+# print(ret)
+
+# ret = re.split('[s,a,d]','aewrsqwerdfwerasdfqwer')
+# print(ret)
+# ret = re.match('asd','asdfqwer')
+# print(ret.group())
+# ret = re.sub('a..x','lipeng','hfjasalexxdhf')
+# print(ret)
+
+# obj = re.compile('\.com')
+# print(obj.findall('asdf.comasdf'))
+
+
+
+
+
+
+
+
+
+
+
