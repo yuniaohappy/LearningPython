11c313d65fdf219f308cfe81604c0385a30f8803
Administrator <lpsddz@126.com>
2019-03-06 22:28:26 +0800

    更新

---
+++ sklearn&tensorflow/housing.py
@@ -0,0 +1,31 @@
+import os
+import tarfile
+from six.moves import urllib
+import pandas as pd
+import matplotlib.pyplot as plt
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + '/housing.tgz'
+
+def fetch_housing_data(housing_url=HOUSING_URL,housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path,"housing.tgz")
+    urllib.request.urlretrieve(housing_url,tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path,"housing.csv")
+    return pd.read_csv(csv_path)
+housing = load_housing_data()
+# print(housing.head())
+# print(housing.info())
+# print(housing["ocean_proximity"].value_counts())
+# print(housing.describe())
+# print(housing["median_house_value"].describe())
+# housing.hist()
+housing.hist(bins=50,figsize=(20,16))
+plt.show()
