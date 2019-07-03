74b10eb2bda825bf36bdbf95fb27f54cc3213427
Li Peng <lpsddz@126.com>
2019-07-03 17:36:42 +0800

    2019-06-26：机器学习提交。

--- Spark/pyspark_test.py
+++ Spark/pyspark_test.py
@@ -1,8 +1,13 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('spark_test').getOrCreate()
-spark.read.json('D:\\spark-2.3.3-bin-hadoop2.7\\examples\\src\\main\\resources\\people.json').show()
-
+people = spark.read.json('D:\\spark-2.3.3-bin-hadoop2.7\\examples\\src\\main\\resources\\people.json')\
+    .createOrReplaceGlobalTempView('people')
+print(type(people))
+user = spark.read.parquet('D:\\spark-2.3.3-bin-hadoop2.7\\examples\\src\\main\\resources\\users.parquet').show()
+user.select('user').show()
+# people.select().show()
+# spark.sql('select * from people').show()
 # from pyspark import SparkConf, SparkContext
 # conf = SparkConf().setMaster("local[*]").setAppName("First_App")
 # sc = SparkContext(conf=conf)
