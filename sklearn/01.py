f67be3ca43f4febfb5c5431f340186642526b9f5
Li Peng <lpsddz@126.com>
2019-07-12 16:36:14 +0800

    2019-07-12：代码目录结构调整。

--- sklearn/01.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction import DictVectorizer
-X = [{'city':'北京','temperature':100},
-     {'city':'上海','temperature':50},
-     {'city':'深圳','temperature':30}]
-dv = DictVectorizer(sparse=False)
-data = dv.fit_transform(X)
-print(data)
-from sklearn import datasets
-iris = datasets.load_iris()
