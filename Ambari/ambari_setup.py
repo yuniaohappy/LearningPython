7f1e674992ce5d239271b13b3ca26304b4c718e9
Li Peng <lpsddz@126.com>
2019-07-13 22:11:30 +0800

    2019-07-13：Ambari 安装流程优化。

--- Ambari/ambari_setup.py
+++ Ambari/ambari_setup.py
@@ -1,10 +1,10 @@
 import json
 import argparse
 import requests
-
+import pprint
 
 class AmbariSetup(object):
-    def __init__(self, host, port, username, password, clustername, blueprintname):
+    def __init__(self, host, port, username, password, clustername, blueprintname,master1,master2,server,slave,hdp,hdputils):
         self.host = host
         self.port = port
         self.username = username
@@ -12,6 +12,13 @@ class AmbariSetup(object):
         self.clustername = clustername
         self.blueprintname = blueprintname
         self.headers = {"X-Requested-By": "ambari"}
+        self.master1 = master1
+        self.master2 = master2
+        self.server = server
+        self.slave = slave.split(',')
+        self.hdp = hdp
+        self.hdputils = hdputils
+
 
     def blue_print(self):
         with open('./json/blueprint.json', 'r') as blue_file:
@@ -19,22 +26,40 @@ class AmbariSetup(object):
             blue_print_json['Blueprints']['blueprint_name'] = self.blueprintname
             return json.dumps(blue_print_json)
 
-    def hdp(self):
-        with open('./json/hdp.json', 'r') as hdp_file:
-            hdp = json.loads(hdp_file.read(), encoding='utf-8')
-            return json.dumps(hdp)
+    # 配置集群的存储库
+    def hdp_hdputils(self,url):
+        if url == 'hdp':
+            hdp = {"Repositories" : {"base_url" : self.hdp,"verify_base_url" : True}}
+        elif url == 'hdputils':
+            hdp = {"Repositories": {"base_url": self.hdputils, "verify_base_url": True}}
+        else:
+            None
+        return json.dumps(hdp)
 
-    def hdputils(self):
-        with open('./json/hdputils.json', 'r') as hdputiles_file:
-            hdputiles = json.loads(hdputiles_file.read(), encoding='utf-8')
-            return json.dumps(hdputiles)
+    # 创建Hadoop集群
+    def create_cluster(self):
+        hostmap = {}
+        hostmap['blueprint'] = self.blueprintname
+        hostmap['default_password'] = self.password
+        hostmap['host_groups'] = self._add_hosts()
+        return hostmap
 
-    def hostmapping(self):
-        with open('./json/hosts.json', 'r') as hostmapping_file:
-            hostmap = json.loads(hostmapping_file.read(), encoding='utf-8')
-            hostmap['blueprint'] = self.blueprintname
-            hostmap['default_password'] = 'lp'
-            return json.dumps(hostmap)
+    def _add_hosts(self):
+        # {"name": "master_1", "hosts": [{ "fqdn": "vm-5.novalocal" }]}
+        host_groups = [
+            {"name": "master_1",
+             "hosts": [{"fqdn": self.master1}]},
+            {"name": "master_1",
+             "hosts": [{"fqdn": self.master2}]},
+            {"name": "server",
+             "hosts": [{"fqdn": self.server}]}
+        ]
+
+        for n in range(len(self.slave)):
+            host_map = {"name": "slave_" + str(n + 1),
+                        "hosts": [{ "fqdn": self.slave[n] }]}
+            host_groups.append(host_map)
+        return host_groups
 
     def get_url(self, name):
         if name == 'blueprint':
@@ -65,10 +90,30 @@ if __name__ == '__main__':
     ap.add_argument('--username', help='Ambari 用户名', default='admin')
     ap.add_argument('--password', help='Ambari 密码', default='admin')
     ap.add_argument('--clustername', help='Ambari 名称', default='ambari_lp')
-    ap.add_argument('--blueprintname', help='bluprint 名称', default='ambari_lp')
+    ap.add_argument('--blueprintname', help='Bluprint 名称', default='ambari_lp')
+    ap.add_argument('--master1',help='Master1 主机名',default='master01')
+    ap.add_argument('--master2',help='Master2 主机名',default='master02')
+    ap.add_argument('--server',help='Server 主机名',default='server')
+    ap.add_argument('--slave',help='Slave 节点列表',default='slave01,slave02,slave03')
+    ap.add_argument('--hdp',help='HDP 存储库配置',default='http://20.5.192.193/HDP')
+    ap.add_argument('--hdputils', help='HDP-UTILS 存储库配置', default='http://20.5.192.193/HDP-UTILS')
+
     args = ap.parse_args()
 
-    ambari = AmbariSetup(args.host, args.port, args.username, args.password, args.clustername, args.blueprintname)
+    ambari = AmbariSetup(args.host,
+                         args.port,
+                         args.username,
+                         args.password,
+                         args.clustername,
+                         args.blueprintname,
+                         args.master1,
+                         args.master2,
+                         args.server,
+                         args.slave,
+                         args.hdp,
+                         args.hdputils)
+    # pprint.pprint(ambari.create_cluster())
+    pprint.pprint(ambari.hdp_hdputils('hdputils'))
 
     # 获取已经创建好的集群blueprint
     # url = 'http://{host}:{port}/api/v1/clusters/{clusterName}?format=blueprint'\
@@ -79,25 +124,27 @@ if __name__ == '__main__':
     #     f.write(json.dumps(data,indent=True))
 
     # print('第一步：创建和配置集群蓝图请求，开始。。。。')
-    print(ambari.get_url('blueprint'))
+    # print(ambari.get_url('blueprint'))
     # blueprint = requests.post(ambari.get_url('blueprint'),json=ambari.blue_print(),headers = ambari.headers,auth=(ambari.username, ambari.password))
     # print(blueprint.text)
-
+    #
+    #
     # print('第二步：创建本地HDP存储库请求（可以执行多次创建不同操作系统类型的本地库），开始。。。。')
-    print(ambari.get_url('hdp'))
-    print('http://20.5.194.63:8080/api/v1/stacks/HDP/versions/2.5/operating_systems/redhat7/repositories/HDP-2.5')
-
-    # hdp = requests.post(ambari.get_url('hdp'), json=ambari.hdp(), headers=ambari.headers,
+    # print(ambari.get_url('hdp'))
+    # print('http://20.5.194.63:8080/api/v1/stacks/HDP/versions/2.5/operating_systems/redhat7/repositories/HDP-2.5')
+    # hdp = requests.put(ambari.get_url('hdp'), json=ambari.hdp(), headers=ambari.headers,
     #                     auth=(ambari.username, ambari.password))
-    # print(hdp.text)
+    # print(hdp.ok)
+    #
+    #
     # print('第三步：创建本地HDP-UTILS存储库请求（可以执行多次创建不同操作系统类型的本地库）')
-    print(ambari.get_url('hdputils'))
-
-    # hdp_utils_url = requests.post(ambari.get_url('hdputils'), json=ambari.hdputils(), headers=ambari.headers,
+    # print(ambari.get_url('hdputils'))
+    # hdp_utils_url = requests.put(ambari.get_url('hdputils'), json=ambari.hdputils(), headers=ambari.headers,
     #                               auth=(ambari.username, ambari.password))
-    # print(hdp_utils_url.text)
-
+    # print(hdp_utils_url.ok)
+    #
+    #
     # print('第四步：使用创建好的蓝图安装部署集群')
-    print(ambari.get_url('cluster'))
+    # print(ambari.get_url('cluster'))
     # host_mapping_url = requests.post(ambari.get_url('cluster'),json=ambari.hostmapping(),headers = ambari.headers,auth=(ambari.username, ambari.password))
     # print(host_mapping_url.text)
